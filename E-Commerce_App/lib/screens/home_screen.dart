f557911909866a6588bb28d733e584d91f76a9d0
Florent <florent.chabin@students.campus.academy>
2021-06-14 14:10:10 +0200

    bottom navbar with navigation

--- E-Commerce_App/lib/screens/home_screen.dart
+++ E-Commerce_App/lib/screens/home_screen.dart
@@ -1,4 +1,4 @@
-import 'package:first_project/screens/profile_screen.dart';
+
 import 'package:flutter/material.dart';
 
 
@@ -11,13 +11,13 @@ class HomeScreen extends StatefulWidget{
 
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _inc = 0;
+  int _selectedIndex = 0;
 
   @override
   void initState(){
     super.initState();
     setState(() {
-      _inc = 10;
+      _selectedIndex = 0;
     });
   }
 
@@ -26,39 +26,69 @@ class _HomeScreenState extends State<HomeScreen> {
     super.dispose();
   }
 
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Accueil',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Favoris',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Shop',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Compte',
+      style: optionStyle,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      body: Container(
-        child: GestureDetector(
-          child: Center(
-            child: Container(
-              child: Column( 
-                children: [
-                  SizedBox(
-                    child: ElevatedButton(
-                      
-                      onPressed: () => setState(() => _inc++),
-                      child: Text("Coucou"),
-                    ),
-                  ),
-                  Text("Nombre de click: $_inc"),
-                  SizedBox(
-                    child: ElevatedButton(
-                      onPressed: () => Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => ProfileScreen()),
-                          (route) => false,
-                        ),
-                      child: Text("Coucou"),
-                    ),
-                  )
-              ],
-              )  
-            )
-           
-            
-          ),
+    return MaterialApp(
+      title: "E-Commerce",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("E-Commerce B2DEV")
+          )
+        ,
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Accueil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: 'Favoris',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shop),
+              label: 'Shop',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Compte',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          onTap: _onItemTapped,
         ),
       )
     );
